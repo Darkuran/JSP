a7daafa1a0ca433bc72da29f4a956c25e43a906e
Darkuran <44945258+Darkuran@users.noreply.github.com>
2019-02-19 19:27:29 +0400

    Add files via upload

---
+++ src/java/servlet/WebServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Zhuravlev Evgeniy
+ */
+public class WebServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("index.html");
+        requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); 
+    }
+    
+    
+}
